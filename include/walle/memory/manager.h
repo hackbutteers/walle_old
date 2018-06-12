ed249dd784e5f21afd28e4136fc27e8749cb716f
walle_group@hotmail.com <walle_group@hotmail.com>
2018-06-12 11:16:55 +0800

    init

--- include/walle/memory/manager.h
+++ include/walle/memory/manager.h
@@ -52,23 +52,6 @@ WALLE_FORCE_INLINE size_t Manager::total() const
     return _total;
 }
 
-WALLE_FORCE_INLINE void Manager::add(size_t cnt)
-{
-    size_t current = (_total += cnt);
-    _peak = std::max(_peak.load(), current);
-    ++_alloc_count;
-    if (_super) {
-        _super->add(cnt);
-    }
-}
-WALLE_FORCE_INLINE void Manager::sub(size_t cnt)
-{
-    WALLE_ASSERT(cnt <= _total, "sub size > total");
-    _total -= cnt;
-    if(_super) {
-        _super->sub(cnt);
-    }
-}
 }
 } //bamespace walle
 #endif //WALLE_MEMORY_MANAGER_H_
--- src/memory/manager.cc
+++ src/memory/manager.cc
@@ -9,6 +9,24 @@ Manager::~Manager()
     }
 }
 
+void Manager::add(size_t cnt)
+{
+    size_t current = (_total += cnt);
+    _peak = std::max(_peak.load(), current);
+    ++_alloc_count;
+    if (_super) {
+        _super->add(cnt);
+    }
+}
+void Manager::sub(size_t cnt)
+{
+    WALLE_ASSERT(cnt <= _total, "sub size > total");
+    _total -= cnt;
+    if(_super) {
+        _super->sub(cnt);
+    }
+}
+
 ManagerPtr g_bypass_manager(new Manager(ManagerPtr(), "Bypass"));
 
 }
