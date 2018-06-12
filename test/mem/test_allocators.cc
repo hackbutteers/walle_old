cffb590c8a7a187124d30611e59791ec8370cc71
walle_group@hotmail.com <walle_group@hotmail.com>
2018-06-12 11:13:12 +0800

    init

--- test/mem/test_allocators.cc
+++ test/mem/test_allocators.cc
@@ -17,12 +17,14 @@ TEST(mem, allocator)
     alloctor1.deallocate(ptr, 1024);
     printf("%lu\n", walle::mem::malloc_tracker_total_allocs());
     {
+        /*
         walle::mem::string a{walle::mem::Allocator<char>(m)};
         a.append(8092, 'c');
         (void)a;
         walle::mem::by_string b("24");
         b.append(8092, 'c');
         (void)b;
+        */
     }
     printf("%lu\n", walle::mem::malloc_tracker_total_allocs());
 }
